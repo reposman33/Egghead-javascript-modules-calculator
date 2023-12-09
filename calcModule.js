e2b8856b0918cd067d925ce0754c927e82425659
Marc <3200870+reposman33@users.noreply.github.com>
2023-12-09 18:33:46 +0000

    update calculator taking into account operator precedence in expression

--- calcModule.js
+++ calcModule.js
@@ -1,56 +1,71 @@
 var calculator = (function () {	
-	var innerFactory = function(onResultUpdated){
+	var innerFactory = function(displayValue){
 		var innerCalc = {};	
-		var currentTotal = 0;
-		var lastVal = 0;
-		var nextOp;
+		let currentTotal = '';
+		let operators = [];
+		let operands = [];
+		const availableOperators = {'+': add, '-': minus, '*': multiply, '/':divide};
+		const operatorsByPrecedence = [multiply,divide,add,minus];
 
-		function add(val){
-			currentTotal = currentTotal + val;
+		function add(leftVal, rightVal){
+			return leftVal + rightVal;
 		}
-		function minus(val) {
-			currentTotal = currentTotal - val;
+		function minus(leftVal, rightVal) {
+			return leftVal - rightVal;
 		}
-		function multiply(val){
-			currentTotal = currentTotal * val;
+		function multiply(leftVal, rightVal){
+			return leftVal * rightVal;
 		}
-		function divide(val){
+		function divide(leftVal, rightVal){
 			if(val != 0){
-				currentTotal = currentTotal / val;
+				return leftVal / rightVal;
 			}
 		}
 
-		function handleNumber(val){
-			val = parseInt(val);
-			lastVal = val;
-			if(nextOp){
-				nextOp(val);
-			} else {
-				currentTotal = val;
-			}	
-			if(onResultUpdated){
-				onResultUpdated(currentTotal);
-			}
+		function getExpressionValue() {
+			// take each operator by precedence...
+			operatorsByPrecedence.forEach((operator, index) => {
+				const opIndex = 1 // yes, it's fake but we need a value here to enter while loop
+				while(opIndex) {
+					// ..find out if this operator is used in the expression
+					const opIndex = operators.findIndex(operator);
+					if (opIndex) {
+						// ...if so, find operands for operator
+						const leftOperand = operands[opIndex];
+						const rightOperand = operands[opIndex+1];
+						// ...replace the 2 operands with the result of the sub expression
+						operands.splice(opIndex, 2, operator(leftOperand, rightOperand));
+						// ...remove used operator from list
+						operators.splice(opIndex,1);
+					}
+				}
+				return operands[0];
+			})
 		}
 
 		innerCalc.input = function(val) {
 			if(val === "C"){
-				currentTotal = 0;
-				if(onResultUpdated){
-					onResultUpdated(currentTotal);
+				currentTotal = '';
+				displayValue(0);
+				operands = [];
+				operators = [];
+				currentTotal = '';
+			} else if(Object.keys(availableOperators).includes(val)) {
+				if(currentTotal !== ''){
+					operands.push(parseInt(currentTotal))
+				};
+				operators.push(availableOperators[val]);
+				displayValue(val);
+				currentTotal = '';
+			} else if(['0','1','2','3','4','5','6','7','8','9'].includes(val)) {
+				currentTotal += val;
+				displayValue(currentTotal);	
+			} else if(val === '=') {
+				if(currentTotal !== ''){
+					operands.push(parseInt(currentTotal));
 				}
-			} else if(val === "=" && nextOp) {
-				handleNumber(lastVal);
-			} else if(val === "+"){
-				nextOp = add;
-			} else if(val === "-") {
-				nextOp = minus;
-			} else if(val === "*") {
-				nextOp = multiply;
-			} else if(val === "/") {
-				nextOp = divide;
-			} else {
-				handleNumber(val);
+				currentTotal = '';
+				displayValue(getExpressionValue());
 			}
 		}
 
